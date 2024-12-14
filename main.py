a2d73f3c26ab966fc490f63e0944302539429980
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-14 17:24:51 +0800

    chore: update port

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ class MainApp:
 # Running the apt apt
 if __name__ == "__main__":
     main_app = MainApp()
-    main_app.run(debug=True, host='0.0.0.0', port=5000)
+    main_app.run(debug=True, host='0.0.0.0', port=2000)
