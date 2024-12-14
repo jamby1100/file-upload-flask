be1e14df62c3210596d45cd8b802788a0b22904d
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-14 16:41:58 +0800

    chore: test hard coded

--- tasks/kafka_producer.py
+++ tasks/kafka_producer.py
@@ -1,22 +1,10 @@
 from kafka import KafkaProducer
-from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 import json
 
-# AWS region where MSK cluster is located
-region = 'ap-southeast-1'
+# Hardcoded token
+hardcoded_token = 'aHR0cHM6Ly9rYWZrYS5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tLz9BY3Rpb249a2Fma2EtY2x1c3RlciUzQUNvbm5lY3QmWC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBNFhONE9STEJBS1BRWlpGNyUyRjIwMjQxMjE0JTJGYXAtc291dGhlYXN0LTElMkZrYWZrYS1jbHVzdGVyJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDEyMTRUMDg0MDA1WiZYLUFtei1FeHBpcmVzPTkwMCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmWC1BbXotU2lnbmF0dXJlPTJiYTcwZmMxZjJmYjA1YWNjNWE4YWI0YjEyNjQxYzQzNDlkMjcyZmNkYTdkMjQxMDY2ZmFkMTc4NDkwMGFhYzYmVXNlci1BZ2VudD1hd3MtbXNrLWlhbS1zYXNsLXNpZ25lci1weXRob24lMkYxLjAuMQ'
 
-# Class to provide MSK authentication token
-class MSKTokenProvider:
-    def token(self):
-        token, _ = MSKAuthTokenProvider.generate_auth_token(region)
-        
-        print(token,'tokengen')
-        return token
-
-# Create an instance of MSKTokenProvider class
-tp = MSKTokenProvider()
-
-# Kafka producer configuration
+# Kafka producer configuration with hardcoded token
 producer = KafkaProducer(
     bootstrap_servers='boot-i0fqmu70.c1.kafka-serverless.ap-southeast-1.amazonaws.com:9098',
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),
@@ -24,7 +12,7 @@ producer = KafkaProducer(
     request_timeout_ms=30000,
     security_protocol='SASL_SSL',
     sasl_mechanism='OAUTHBEARER',
-    sasl_oauth_token_provider='aHR0cHM6Ly9rYWZrYS5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tLz9BY3Rpb249a2Fma2EtY2x1c3RlciUzQUNvbm5lY3QmWC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBNFhONE9STEJBS1BRWlpGNyUyRjIwMjQxMjE0JTJGYXAtc291dGhlYXN0LTElMkZrYWZrYS1jbHVzdGVyJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDEyMTRUMDg0MDA1WiZYLUFtei1FeHBpcmVzPTkwMCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmWC1BbXotU2lnbmF0dXJlPTJiYTcwZmMxZjJmYjA1YWNjNWE4YWI0YjEyNjQxYzQzNDlkMjcyZmNkYTdkMjQxMDY2ZmFkMTc4NDkwMGFhYzYmVXNlci1BZ2VudD1hd3MtbXNrLWlhbS1zYXNsLXNpZ25lci1weXRob24lMkYxLjAuMQ',
+    sasl_oauth_token_provider=lambda: hardcoded_token,  # Using the hardcoded token
 )
 
 # Function to send resize task to Kafka
