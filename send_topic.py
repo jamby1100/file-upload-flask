7ba490207c3847a7be444d0c921383b5785230cd
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-14 22:07:40 +0800

    chore: change import

--- send_topic.py
+++ send_topic.py
@@ -1,8 +1,8 @@
-from confluent_kafka import AdminClient, NewTopic
+from kafka.admin import KafkaAdminClient, NewTopic
 from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 
 # AWS region where MSK cluster is located
-region = 'ap-southeast-1'
+region= '<UPDATE_AWS_REGION_NAME_HERE>'
 
 # Class to provide MSK authentication token
 class MSKTokenProvider():
@@ -13,26 +13,21 @@ class MSKTokenProvider():
 # Create an instance of MSKTokenProvider class
 tp = MSKTokenProvider()
 
-# Initialize AdminClient with required configurations
-conf = {
-    'bootstrap.servers': 'boot-i0fqmu70.c1.kafka-serverless.ap-southeast-1.amazonaws.com:9098',  
-    'security.protocol': 'SASL_SSL',
-    'sasl.mechanisms': 'AWS_MSK_IAM',  # Use AWS_MSK_IAM for IAM authentication
-    'sasl.username': '',  # Leave blank as IAM is used
-    'sasl.password': '',  # Leave blank as IAM is used
-    'client.id': 'client1',
-    'sasl.token.provider': tp  # Provide the token provider
-}
+# Initialize KafkaAdminClient with required configurations
+admin_client = KafkaAdminClient(
+    bootstrap_servers='<UPDATE_BOOTSTRAP_SERVER_STRING_HERE>',
+    security_protocol='SASL_SSL',
+    sasl_mechanism='OAUTHBEARER',
+    sasl_oauth_token_provider=tp,
+    client_id='client1',
+)
 
-admin_client = AdminClient(conf)
-
-# Create a topic
-topic_name = "mytopic"
-topic_list = [NewTopic(name=topic_name, num_partitions=1, replication_factor=2)]
-
+# create topic
+topic_name="mytopic"
+topic_list =[NewTopic(name=topic_name, num_partitions=1, replication_factor=2)]
 existing_topics = admin_client.list_topics()
-if topic_name not in existing_topics:
+if(topic_name not in existing_topics):
     admin_client.create_topics(topic_list)
     print("Topic has been created")
 else:
-    print(f"Topic already exists! List of topics are: {existing_topics}")
+    print("topic already exists!. List of topics are:" + str(existing_topics))
