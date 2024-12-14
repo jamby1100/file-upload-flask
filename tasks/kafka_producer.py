74dbc1ca74ecf726a392ebdc9a491e222271ffcf
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-14 16:44:16 +0800

    chore: add expiry token

--- tasks/kafka_producer.py
+++ tasks/kafka_producer.py
@@ -1,10 +1,33 @@
 from kafka import KafkaProducer
+from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 import json
+import time
 
-# Hardcoded token
-hardcoded_token = 'aHR0cHM6Ly9rYWZrYS5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tLz9BY3Rpb249a2Fma2EtY2x1c3RlciUzQUNvbm5lY3QmWC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBNFhONE9STEJBS1BRWlpGNyUyRjIwMjQxMjE0JTJGYXAtc291dGhlYXN0LTElMkZrYWZrYS1jbHVzdGVyJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDEyMTRUMDg0MDA1WiZYLUFtei1FeHBpcmVzPTkwMCZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmWC1BbXotU2lnbmF0dXJlPTJiYTcwZmMxZjJmYjA1YWNjNWE4YWI0YjEyNjQxYzQzNDlkMjcyZmNkYTdkMjQxMDY2ZmFkMTc4NDkwMGFhYzYmVXNlci1BZ2VudD1hd3MtbXNrLWlhbS1zYXNsLXNpZ25lci1weXRob24lMkYxLjAuMQ'
+# AWS region where MSK cluster is located
+region = 'ap-southeast-1'
 
-# Kafka producer configuration with hardcoded token
+# Class to provide MSK authentication token with caching mechanism
+class MSKTokenProvider:
+    def __init__(self):
+        self.token = None
+        self.token_expiry_time = 60  # Time when the token expires (in seconds)
+
+    def token(self):
+        # Check if the current token is expired
+        if self.token is None or time.time() >= self.token_expiry_time:
+            # Generate a new token if it's expired or not set
+            token, expiry_time = MSKAuthTokenProvider.generate_auth_token(region)
+            self.token = token
+            self.token_expiry_time = time.time() + expiry_time  # Expiry time in seconds
+            print("New token generated")
+        else:
+            print("Using cached token")
+        return self.token
+
+# Create an instance of MSKTokenProvider class
+tp = MSKTokenProvider()
+
+# Kafka producer configuration
 producer = KafkaProducer(
     bootstrap_servers='boot-i0fqmu70.c1.kafka-serverless.ap-southeast-1.amazonaws.com:9098',
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),
@@ -12,7 +35,7 @@ producer = KafkaProducer(
     request_timeout_ms=30000,
     security_protocol='SASL_SSL',
     sasl_mechanism='OAUTHBEARER',
-    sasl_oauth_token_provider=lambda: hardcoded_token,  # Using the hardcoded token
+    sasl_oauth_token_provider=tp,
 )
 
 # Function to send resize task to Kafka
@@ -28,3 +51,6 @@ def send_resize_task(file_path, width, height):
     producer.send('image-resize', task)
     producer.flush()  # Ensure the message is sent
     print(f"Resize task sent: {task}")
+
+# Example usage
+send_resize_task('path/to/file.jpg', 800, 600)
