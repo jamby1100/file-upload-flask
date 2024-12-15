06a82181f66d287b5fe0f1468d07b772a61912ba
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-16 01:26:47 +0800

    feat: loop consume_message

--- receive_resize_image_kafka.py
+++ receive_resize_image_kafka.py
@@ -50,14 +50,14 @@ def consume_message( topic_name='your-topic-name', timeout=5.0):
     finally:
         consumer.close()
 
-# def consume_message_loop(topic_name='your-topic-name', timeout=5.0):
-#     while True:
-#         result = consume_message(topic_name, timeout)
-#         if result == "No message received":
-#             print("No messages received, stopping.")
-#             break
-#         else:
-#             print(f"Processed: {result}")
+def consume_message_loop(topic_name='your-topic-name', timeout=5.0):
+    while True:
+        result = consume_message(topic_name, timeout)
+        if result == "No message received":
+            print("No messages received, stopping.")
+            break
+        else:
+            print(f"Processed: {result}")
 
 def resize_and_upload_image(file_path,image_mongo_id, width, height):
     resized_path = file_path.replace(".", "_resized.")
