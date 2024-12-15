8d1aa8ce89fd9301852fb6eb36b5b97b16140433
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-15 11:46:20 +0800

    fix: unindent send

--- actions/send_kafka.py
+++ actions/send_kafka.py
@@ -1,38 +1,34 @@
-  import json
-  import time
-  from time import time
-  from confluent_kafka import Producer
-  from PIL import Image
+import json
+import time
+from time import time
+from confluent_kafka import Producer
+from PIL import Image
 
-  producer = Producer({
-      'bootstrap.servers': 'b-3.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,b-1.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,b-2.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092',
-      'socket.timeout.ms': 100,
-      'api.version.request': 'false',
-      'broker.version.fallback': '0.9.0',
-      'message.max.bytes': 1000000000
-  })
-
-
-
-  # data = {"name": "xyz", "email": "xyz@"}
+producer = Producer({
+    'bootstrap.servers': 'b-3.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,b-1.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,b-2.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092',
+    'socket.timeout.ms': 100,
+    'api.version.request': 'false',
+    'broker.version.fallback': '0.9.0',
+    'message.max.bytes': 1000000000
+})
 
 
+# data = {"name": "xyz", "email": "xyz@"}
 
 
-  def send_msg_async(msg,id):
-      # print("Sending message")
-      msg["id"] = id
-      try:
-        
-          msg_json_str = str(json.dumps(msg))
-          producer.produce(
-              'your-topic-name',
-              msg_json_str
-          )
-          print('sent-message', msg_json_str)
-          producer.flush()
-          return "Message sent successfully."
-      except Exception as ex:
-          print("Error : ", ex)
-          return f"Failed to send message: {ex}"
-  # send_msg_async(data)
+def send_msg_async(msg, id):
+    # print("Sending message")
+    
+    try:
+        msg["id"] = id
+        msg_json_str = str(json.dumps(msg))
+        producer.produce(
+            'your-topic-name',
+            msg_json_str
+        )
+        print('sent-message', msg_json_str)
+        producer.flush()
+        return "Message sent successfully."
+    except Exception as ex:
+        print("Error : ", ex)
+        return f"Failed to send message: {ex}"
