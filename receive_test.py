67e107af598ada4fcb5e77261ba781f7cffff3c1
Elisha Necesito <elisha.necesito@ecloudvalley.com>
2024-12-15 09:05:51 +0800

    chore: adjust timeout

--- receive_test.py
+++ receive_test.py
@@ -4,8 +4,8 @@ consumer = Consumer({
     'bootstrap.servers': 'b-3.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,'
                          'b-1.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092,'
                          'b-2.unauth.xbyahs.c3.kafka.ap-southeast-1.amazonaws.com:9092',
-    'socket.timeout.ms': 100,
-      'fetch.wait.max.ms':90,
+    'socket.timeout.ms': 1000,
+      'fetch.wait.max.ms':900,
     'api.version.request': 'false',
     'broker.version.fallback': '0.9.0',
     'message.max.bytes': 1000000000,
